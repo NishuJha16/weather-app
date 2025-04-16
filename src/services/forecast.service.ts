d3ccd9084fa9564861e982959dc65efaf3ad1df4
NishuJha16 <nishujha088@gmail.com>
2025-04-16 13:24:58 +0530

    Update forecast.service.ts

--- src/services/forecast.service.ts
+++ src/services/forecast.service.ts
@@ -2,7 +2,7 @@ import service from ".";
 
 export const getCurrentDayData = async (params: any) => {
   try {
-    const response = await service.get("forecast.json", { params: params });
+    const response = await service.get("forecast", { params: params });
     return response.data;
   } catch (error) {
     console.error(error);
